edaf376848cf8ebe49b3f5c7a330bc2272d6ec22
Sina Ghaffari <sinaghafari.dev@gmail.com>
2025-11-14 12:32:08 +0330

    create loading page

--- frontend/app/loading.tsx
+++ frontend/app/loading.tsx
@@ -1,4 +1,11 @@
+'use client';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
 export default function Loading() {
-  // Or a custom loading skeleton component
-  return <p>Loading...</p>
+  return (
+    <div className="flex flex-col items-center justify-center space-y-4 py-20 h-screen">
+      <DotLottieReact src="/images/NotFound.json" autoplay loop />
+      <p className="text-lg text-gray-500">Loading...</p>
+    </div>
+  );
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,6 @@
 import TrashIcon from '@/public/icons/TrashIcon';
 import { Button } from '../components/ui/button';
+import Loading from './loading';
 
 export default function Home() {
   return <div>home page</div>;
